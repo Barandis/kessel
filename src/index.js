22aa661fa1c342b999062a96b2f87076a62f83df
Thomas Otterson <barandis1729@gmail.com>
2020-11-08 22:08:48 -0500

    feat(project): add index file for API, do first build

---
+++ src/index.js
@@ -0,0 +1,136 @@
+// Copyright (c) 2020 Thomas J. Otterson
+//
+// This software is released under the MIT License.
+// https://opensource.org/licenses/MIT
+
+///////////////////////////////////////////////////////////////////////
+// PARSERS
+
+export {
+  alpha,
+  any,
+  anyOf,
+  char,
+  chari,
+  digit,
+  eof,
+  hex,
+  letter,
+  lower,
+  noneOf,
+  octal,
+  range,
+  satisfy,
+  upper,
+} from './parsers/char'
+export {
+  constant,
+  fail,
+  failFatally,
+  unexpected,
+} from './parsers/misc'
+export {
+  alphaU,
+  letterU,
+  lowerU,
+  newline,
+  newlineU,
+  regex,
+  space,
+  spaceU,
+  spaces,
+  spaces1,
+  spaces1U,
+  spacesU,
+  upperU,
+} from './parsers/regex'
+export {
+  all,
+  anyString,
+  string,
+  stringi,
+} from './parsers/string'
+
+///////////////////////////////////////////////////////////////////////
+// COMBINATORS
+
+export {
+  alt,
+  altL,
+  back,
+  bothB,
+  chainB,
+  countB,
+  leftB,
+  manyTillB,
+  optional,
+  orElse,
+  rightB,
+  seqB,
+} from './combinators/alternative'
+export {
+  between,
+  both,
+  chain,
+  join,
+  left,
+  map,
+  pipe,
+  right,
+  skip,
+  value,
+} from './combinators/chaining'
+export {
+  followedBy,
+  followedByL,
+  lookAhead,
+  notEmpty,
+  notFollowedBy,
+  notFollowedByL,
+} from './combinators/conditional'
+export {
+  label,
+} from './combinators/message'
+export {
+  block,
+  chainl,
+  chainl1,
+  chainr,
+  chainr1,
+  count,
+  many,
+  many1,
+  manyTill,
+  sepBy,
+  sepBy1,
+  sepEndBy,
+  sepEndBy1,
+  seq,
+  skipMany,
+  skipMany1,
+} from './combinators/sequence'
+
+///////////////////////////////////////////////////////////////////////
+// PARSER TOOLS AND UTILITIES
+
+export {
+  Status,
+  error,
+  fatal,
+  makeParser,
+  maybeFatal,
+  ok,
+  parse,
+} from './core'
+export {
+  ErrorType,
+  clear,
+  expectedError,
+  formatErrors,
+  genericError,
+  getPosition,
+  otherError,
+  overwrite,
+  push,
+  unexpectedError,
+} from './error'
