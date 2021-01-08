9df1d38828b35bf1486c47c4b4c5a67c4df1c72a
Thomas Otterson <barandis1729@gmail.com>
2021-01-08 03:42:52 -0500

    feat(errors): capitalize first letter in compound errors

--- test/specs/error.js
+++ test/specs/error.js
@@ -628,7 +628,7 @@ describe('Parse errors', () => {
         const exp = 'Parse error at (line 1, column 1):\n\n'
           + 'tesl\n'
           + '^\n\n'
-          + "the word 'test' could not be parsed because:\n\n"
+          + "The word 'test' could not be parsed because:\n\n"
           + '  Parse error at (line 1, column 4):\n\n'
           + '  tesl\n'
           + '     ^\n'
